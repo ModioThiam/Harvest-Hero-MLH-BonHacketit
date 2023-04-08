738c7498d843fd310337e32eaa1e85fa74a1d322
Fayaz Rafin <fayaz.rafin@gmail.com>
2023-04-08 01:52:32 -0400

    fixed mongodb issues

---
+++ backend/connect.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://new_user_fa:abcd1234@foodwasteappcluster.agzjn6e.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
